b498b9128f060d040d51e440a8c96284f1d17a09
Nishat2708 <nishantkumar.patel105184@marwadiuniversity.ac.in>
2022-10-13 04:06:25 +0530

    remove comments

--- hard_codes.py
+++ hard_codes.py
@@ -1,20 +1,20 @@
-# input = input("Input the  temperature you like to convert : ")
-# temp = (input[:-1])
-# unit = input[-1] 
+input = input("Input the  temperature you like to convert : ")
+temp = (input[:-1])
+unit = input[-1] 
 
-# def convert(temp, unit):
-#     unit = unit.lower()
-#     if unit == "c" and temp != "":
-#         temp = 9.0 / 5.0 * int(temp) + 32
-#         return temp
+def convert(temp, unit):
+    unit = unit.lower()
+    if unit == "c" and temp != "":
+        temp = 9.0 / 5.0 * int(temp) + 32
+        return temp
     
-#     elif unit == "f" and temp != "":
-#         temp = (int(temp) - 32)  / 9.0 * 5.0
-#         return temp
-#     else:
-#         r = "Wrong input"
-#         return r
-# print(convert(temp, unit))
+    elif unit == "f" and temp != "":
+        temp = (int(temp) - 32)  / 9.0 * 5.0
+        return temp
+    else:
+        r = "Wrong input"
+        return r
+print(convert(temp, unit))
 
 
 
@@ -50,85 +50,49 @@ else:
 # ---------------------------------------------------
 
 
-# # ------Fibonacci triangle
+# ------Fibonacci triangle
  
-# def fiboTriangle(n):
+def fiboTriangle(n):
  
     
-#     f=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-#     # f=[1,4,9,16,25,36,49,64,91,100]
+    f=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    # f=[1,4,9,16,25,36,49,64,91,100]
  
-#     fiboNum = 0
+    fiboNum = 0
  
-#     for i in range(1, n + 1):
-#         for space in range(1, (n-i)+1):
-#             print(' ',end=" ")
-#         for j in range(1, i + 1):
+    for i in range(1, n + 1):
+        for space in range(1, (n-i)+1):
+            print(' ',end=" ")
+        for j in range(1, i + 1):
  
-#             print(f[fiboNum], " ", end="")
-#             fiboNum = fiboNum + 1
+            print(f[fiboNum], " ", end="")
+            fiboNum = fiboNum + 1
  
-#         print()
+        print()
 
-# n = 4
-# fiboTriangle(n)
+n = 4
+fiboTriangle(n)
 
 
 # ---------------------------------------------------
 
 
 
-# row_size = 4
-# # row_size = int(input("Enter the row size: "))
-# a = 0
-# b = 1
-# c = a+b
-# for i in range(1, row_size+1):
-#     for j in range(1, (row_size-i)+1):
-#         print(end=" ")
-#     for inn in range(1, i+1):
-#         print(c, end=" ")
+row_size = 4
+# row_size = int(input("Enter the row size: "))
+a = 0
+b = 1
+c = a+b
+for i in range(1, row_size+1):
+    for j in range(1, (row_size-i)+1):
+        print(end=" ")
+    for inn in range(1, i+1):
+        print(c, end=" ")
 
-#         c = a+b
-#         a = b
-#         b = c
-#     print("")
-
-
-
-# ---------------------------------------------------
-
-
-
-# row_size = 3
-# a = 0
-# b = 1
-# c = a+b
-# t=1
-# for i in range(1, row_size+1):
-#     for j in range(1, (row_size-i)+1):
-#         print(end=" ")
-#     for inn in range(1, i+1):
-#       if t==1:
-#         print(a, end=" ")
-#         t = t+1
-#       else:
-#         print(c, end=" ")
-
-#         c = a+b
-#         a = b
-#         b = c
-#     print("")
-# for i in range(row_size-1, 0, -1):
-#     for j in range(row_size-i-1):
-#         print(end=" ")
-#     for j in range(0,i):
-#         print(c, end=" ")
-#         c = a+b
-#         a = b
-#         b = c
-#     print("")
-
+        c = a+b
+        a = b
+        b = c
+    print("")
 
 
 
@@ -136,68 +100,108 @@ else:
 
 
 
+row_size = 3
+a = 0
+b = 1
+c = a+b
+t=1
+for i in range(1, row_size+1):
+    for j in range(1, (row_size-i)+1):
+        print(end=" ")
+    for inn in range(1, i+1):
+      if t==1:
+        print(a, end=" ")
+        t = t+1
+      else:
+        print(c, end=" ")
 
-# import pandas as pd
-# class State:
-
-#     state_train = {"Bihar":[23500, 17237, 5927],
-#             "Orissa":[22658, 24555, 2364]
-#             }
-#     def __init__(self):
-#         return self.state_train
-
-#     state_bus = {
-#         "U.P": [36517, 22617, 6314],
-#         "Jharkhand": [23254, 19845, 1336]
-#     }
-#     def __init__(self):
-#         return self.state_bus
-
-
-# class Transport(State):
-
-#      def __init__(self, transport):
-#         if transport == "Train" or transport == "train":
-#             df = pd.DataFrame(self.state_bus).T
-#             df.reset_index(inplace=True)
-#             print("By Bus\n"  + "--------------- ")
-#             df.columns = ['Destination state','Men','Women','Women']
-#             print(df)
-
-#         else:
-#             df = pd.DataFrame(self.state_train).T
-#             df.reset_index(inplace=True)
-#             print("By Train\n"  + "--------------- ")
-#             df.columns = ['Destination state','Men','Women','Women']
-#             print(df)
+        c = a+b
+        a = b
+        b = c
+    print("")
+for i in range(row_size-1, 0, -1):
+    for j in range(row_size-i-1):
+        print(end=" ")
+    for j in range(0,i):
+        print(c, end=" ")
+        c = a+b
+        a = b
+        b = c
+    print("")
 
 
-# a = Transport("train")
-# a = Transport("bus")
 
 
-# n = 9
-# a = 0
-# b = 1
+# ---------------------------------------------------
+
+
+
+
+import pandas as pd
+class State:
+
+    state_train = {"Bihar":[23500, 17237, 5927],
+            "Orissa":[22658, 24555, 2364]
+            }
+    def __init__(self):
+        return self.state_train
+
+    state_bus = {
+        "U.P": [36517, 22617, 6314],
+        "Jharkhand": [23254, 19845, 1336]
+    }
+    def __init__(self):
+        return self.state_bus
+
+
+class Transport(State):
+
+     def __init__(self, transport):
+        if transport == "Train" or transport == "train":
+            df = pd.DataFrame(self.state_bus).T
+            df.reset_index(inplace=True)
+            print("By Bus\n"  + "--------------- ")
+            df.columns = ['Destination state','Men','Women','Women']
+            print(df)
+
+        else:
+            df = pd.DataFrame(self.state_train).T
+            df.reset_index(inplace=True)
+            print("By Train\n"  + "--------------- ")
+            df.columns = ['Destination state','Men','Women','Women']
+            print(df)
+
+
+a = Transport("train")
+a = Transport("bus")
+
+
+
+# ---------------------------------------------------
+
+
+n = 9
+a = 0
+b = 1
      
-#     # Check is n is less
-#     # than 0
-# if n < 0:
-#     print("Incorrect input")
+    # Check is n is less
+    # than 0
+if n < 0:
+    print("Incorrect input")
         
-# # Check is n is equal
-# # to 0
-# elif n == 0:
-#     print (0)
+# Check is n is equal
+# to 0
+elif n == 0:
+    print (0)
     
-# # Check if n is equal to 1
-# elif n == 1:
-#     print (b)
-# else:
-#     for i in range(1, n):
-#         c = a + b
-#         a = b
-#         b = c
-#         print(b)
+# Check if n is equal to 1
+elif n == 1:
+    print (b)
+else:
+    for i in range(1, n):
+        c = a + b
+        a = b
+        b = c
+        print(b)
     
 # Driver Program
--- test2.py
+++ test2.py
@@ -1,13 +1,13 @@
-# def tri_recursion(k):
-#   if(k > 0):
-#     result = k + tri_recursion(k - 1)
-#     print(result)
-#   else:
-#     result = 0
-#   return result
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
 
-# print("\n\nRecursion Example Results")
-# tri_recursion(6)
+print("\n\nRecursion Example Results")
+tri_recursion(6)
 
 
 def factorial(x):
